460440e629c374c1de0a450aaab0663890bab151
Samuel Maurice <contact@samuelmaurice.com>
2021-04-21 12:14:06 +0200

    Add connection with MySqlConnector

--- Sculptor/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sculptor
-{
-    public class Class1
-    {
-    }
-}
---
+++ Sculptor/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Sculptor
+{
+    public static class Connection
+    {
+        public static string ConnectionString { get; set; }
+
+        public static void Execute(string query)
+        {
+            using var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            using var command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+
+        public static async Task ExecuteAsync(string query)
+        {
+            using var connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+
+            using var command = new MySqlCommand(query, connection);
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
