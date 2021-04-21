e56adfee7ab3f90b8694b4bd6f236120d4a5f623
Samuel Maurice <contact@samuelmaurice.com>
2021-04-22 00:36:58 +0200

    Add prepared statements

--- Sculptor/Connection.cs
+++ Sculptor/Connection.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using MySqlConnector;
 
 namespace Sculptor
@@ -7,22 +8,31 @@ namespace Sculptor
     {
         public static string ConnectionString { get; set; }
 
-        public static void Execute(string query)
+        public static void Execute(string query, Dictionary<string, dynamic> parameters = null)
         {
             using var connection = new MySqlConnection(ConnectionString);
             connection.Open();
 
             using var command = new MySqlCommand(query, connection);
+            BindParameters(command, parameters);
             command.ExecuteNonQuery();
         }
 
-        public static async Task ExecuteAsync(string query)
+        public static async Task ExecuteAsync(string query, Dictionary<string, dynamic> parameters = null)
         {
             using var connection = new MySqlConnection(ConnectionString);
             await connection.OpenAsync();
 
             using var command = new MySqlCommand(query, connection);
+            BindParameters(command, parameters);
             await command.ExecuteNonQueryAsync();
+        }
+
+        private static void BindParameters(MySqlCommand command, Dictionary<string, dynamic> parameters = null)
+        {
+            if (parameters != null)
+                foreach (var parameter in parameters)
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
         }
     }
 }
