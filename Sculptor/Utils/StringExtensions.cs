d53f63230ff8c4862070c839e90e90f3a788edc3
Samuel Maurice <contact@samuelmaurice.com>
2021-04-27 12:58:07 +0200

    Add string extensions and the dumbest pluralizer in the world

---
+++ Sculptor/Utils/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sculptor.Utils
+{
+    public static class StringExtensions
+    {
+        public static string Pluralize(this string str)
+        {
+            return str + "s";
+        }
+
+        public static string ToSnakeCase(this string str)
+        {
+            return Regex.Replace(str, "[A-Z]", "_$0").ToLower().TrimStart('_');
+        }
+    }
+}
