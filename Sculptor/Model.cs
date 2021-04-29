2f4c13a1120ba4ea0ba4452200b8d19e9ed60b84
Samuel Maurice <contact@samuelmaurice.com>
2021-04-29 20:45:12 +0200

    Improve save() method to update the record if the model already exists in the database

--- Sculptor/Model.cs
+++ Sculptor/Model.cs
@@ -16,17 +16,51 @@ namespace Sculptor
 
         public void Save()
         {
-            Connection.Execute(CompileInsert().Query, CompileInsert().Parameters);
-            this.GetType().GetProperty("Id").SetValue(this, Connection.LastInsertId);
+            if (Convert.ToInt32(this.GetType().GetProperty("Id").GetValue(this)) == 0)
+            {
+                Connection.Execute(CompileInsert().Query, CompileInsert().Parameters);
+                this.GetType().GetProperty("Id").SetValue(this, Connection.LastInsertId);
+            }
+            else
+            {
+                Connection.Execute(CompileUpdate().Query, CompileUpdate().Parameters);
+            }
         }
 
         public async Task SaveAsync()
         {
-            await Connection.ExecuteAsync(CompileInsert().Query, CompileInsert().Parameters);
-            this.GetType().GetProperty("Id").SetValue(this, Connection.LastInsertId);
+            if (Convert.ToInt32(this.GetType().GetProperty("Id").GetValue(this)) == 0)
+            {
+                await Connection.ExecuteAsync(CompileInsert().Query, CompileInsert().Parameters);
+                this.GetType().GetProperty("Id").SetValue(this, Connection.LastInsertId);
+            }
+            else
+            {
+                await Connection.ExecuteAsync(CompileUpdate().Query, CompileUpdate().Parameters);
+            }
         }
 
         private (string Query, Dictionary<string, dynamic> Parameters) CompileInsert()
+        {
+            Dictionary<string, dynamic> parameters = GetParameters();
+
+            string columns = String.Join(", ", parameters.Keys);
+            string values = String.Join(", ", parameters.Select(p => "@" + p.Key));
+
+            return (String.Format("INSERT INTO {0} ({1}) VALUES ({2})", Table, columns, values), parameters);
+        }
+
+        private (string Query, Dictionary<string, dynamic> Parameters) CompileUpdate()
+        {
+            Dictionary<string, dynamic> parameters = GetParameters();
+
+            string columns = String.Join(", ", parameters.Select(p => String.Format("{0} = @{0}", p.Key)));
+            int id = Convert.ToInt32(this.GetType().GetProperty("Id").GetValue(this));
+
+            return (String.Format("UPDATE {0} SET {1} WHERE id = {2}", Table, columns, id), parameters);
+        }
+
+        private Dictionary<string, dynamic> GetParameters()
         {
             PropertyInfo[] properties = this.GetType().GetProperties();
             Dictionary<string, dynamic> parameters = new Dictionary<string, dynamic>();
@@ -37,7 +71,7 @@ namespace Sculptor
                 if (!bannedProperties.Any(property.Name.Contains))
                     parameters.Add(property.Name.ToSnakeCase(), property.GetValue(this));
 
-            return (String.Format("INSERT INTO {0} ({1}) VALUES ({2})", Table, String.Join(", ", parameters.Keys), String.Join(", ", parameters.Select(p => "@" + p.Key))), parameters);
+            return parameters;
         }
     }
 }
